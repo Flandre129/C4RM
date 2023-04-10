d9285f53a0cf7636a695fbd6cfe2605670dd058e
hy2785 <122698011+Flandre129@users.noreply.github.com>
2023-04-10 14:54:58 -0400

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,11 +1,3 @@
-
-
-y = 0.03
-face = 2000000
-couponRate = 0.04
-m = 10
-
-
 def getBondPrice(y,face,couponRate,m,ppy):
  coupon=face*couponRate/ppy
  pvcf=0
@@ -14,8 +6,3 @@ def getBondPrice(y,face,couponRate,m,ppy):
   pvcfsum=coupon*((1+y/ppy)**(-m*ppy))+face*((1+y/ppy)**(-m*ppy))
   bondPrice=pvcf+pvcfsum
  return bondPrice
-
-bondPrice1=getBondPrice(y,face,couponRate,m,ppy=1)
-print(bondPrice1)
-bondPrice2=getBondPrice(y,face,couponRate,m,ppy=2)
-print(bondPrice2)
